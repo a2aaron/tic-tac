b27e38dbb0fd95e456a5d6344765325c219842bd
Caleb Jones <code@calebjones.net>
2017-12-17 13:37:27 -0500

    Stub out the API

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,23 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+use std::io::{Read, Write};
+
+/// A piece of compiled code that's ready to be evaluated.
+pub struct Program {}
+
+/// Represents failures during execution.
+///
+/// Use it to get access to the cause, backtraces, etc.
+pub struct EvalError {}
+
+impl Program {
+    /// Evaluate a program with given I/O buffers.
+    pub fn eval<R: Read, W: Write>(
+        &self,
+        _input: &mut R,
+        _output: &mut W,
+    ) -> Result<(), EvalError> {
+        unimplemented!();
     }
 }
+
+#[cfg(test)]
+mod tests {}
