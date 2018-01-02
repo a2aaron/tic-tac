ab503a43a93bb2dbe1ee505166a4ad05dac5c0bf
Aaron Kofsky <a2aarontothe2@gmail.com>
2018-01-02 17:30:11 -0500

    Implement expression compilation for literals

--- src/ast/mod.rs
+++ src/ast/mod.rs
@@ -66,27 +66,64 @@ pub struct FunctionCtx {
     max_reg: Addr,
 }
 
-type Block<N> = Vec<Stmt<N>>;
-
 impl FunctionCtx {
-    fn push_tmp(&mut self) -> Addr {
-        unimplemented!()
+    pub fn new() -> FunctionCtx {
+        FunctionCtx {
+            vars: HashMap::new(),
+            consts: Vec::new(),
+            free_reg: 0,
+            max_reg: 0,
+        }
     }
 
-    fn pop_tmp(&mut self, _addr: Addr) {
-        unimplemented!()
+    fn push_tmp(&mut self) -> Addr {
+        let reg = self.free_reg;
+        self.free_reg += 1;
+        if self.free_reg > self.max_reg {
+            self.max_reg = self.free_reg;
+        }
+        reg
     }
+
+    fn pop_tmp(&mut self, addr: Addr) {
+        assert_eq!(self.free_reg, addr + 1);
+        self.free_reg = addr;
+    }
+
+    fn get_const(&mut self, val: &Val) -> Addr {
+        for (i, k) in self.consts.iter().enumerate() {
+            if k == val {
+                return i as u8;
+            }
+        }
+        self.consts.push(val.clone());
+        (self.consts.len() - 1) as u8
+    }
+
     /// Returns a tuple containing the register with the result of the expr
     /// and a Vect of Instrs that generate the expression
-    pub fn compile_expr(&mut self, _expr: Expr<Name>) -> (Addr, Vec<Instr>) {
+    pub fn compile_expr(&mut self, expr: &Expr<Name>) -> (Addr, Vec<Instr>) {
+        use self::Expr::*;
+        match *expr {
+            Lit(ref val) => {
+                let reg = self.push_tmp();
+                let instr = Instr::Const(reg, self.get_const(val));
+                (reg, vec![instr])
+            }
+            Var(ref name) => unimplemented!(),
+            Unop(ref op, ref arg) => unimplemented!(),
+            Binop(ref op, ref left, ref right) => unimplemented!(),
+            Call(ref func, ref args) => unimplemented!(),
+            Index(ref tup, ref idx) => unimplemented!(),
+            Mktup(ref parts) => unimplemented!(),
+        }
+    }
+
+    pub fn compile_stmt(&mut self, _stmt: &Stmt<Name>) -> Vec<Instr> {
         unimplemented!()
     }
 
-    pub fn compile_stmt(&mut self, _stmt: Stmt<Name>) -> Vec<Instr> {
-        unimplemented!()
-    }
-
-    pub fn compile(&mut self, _code: Block<Name>) -> Vec<Instr> {
+    pub fn compile(&mut self, _code: &[Stmt<Name>]) -> Vec<Instr> {
         unimplemented!()
     }
 }
--- src/ast/tests.rs
+++ src/ast/tests.rs
@@ -1,1 +1,13 @@
 use super::*;
+
+#[test]
+fn test_compile_expr() {
+    use bytecode::Instr::*;
+    use bytecode::Val::*;
+
+    let expr = Expr::Lit(I(42));
+    let mut ctx = FunctionCtx::new();
+
+    let result = (0, vec![Const(0, 0)]);
+    assert_eq!(ctx.compile_expr(&expr), result);
+}
